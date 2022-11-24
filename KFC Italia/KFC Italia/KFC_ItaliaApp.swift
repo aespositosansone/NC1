6b1f6cd973c186d56c38efd3877b45fcb77165ba
aespositosansone <aespositosansone22@fed.idserve.net>
2022-11-24 16:38:27 +0100

    dd

---
+++ KFC Italia/KFC Italia/KFC_ItaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KFC_ItaliaApp.swift
+//  KFC Italia
+//
+//  Created by Andrea Esposito Sansone on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct KFC_ItaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
